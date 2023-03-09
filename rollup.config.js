31ce500e6ab73e277da87d8e88b3f0f2e337f4ff
Christoph Reiter <reiter.christoph@gmail.com>
2023-03-09 12:13:54 +0100

    Port to glob v9

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import path from 'path';
 import url from 'url';
-import glob from 'glob';
+import {globSync} from 'glob';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import copy from 'rollup-plugin-copy';
@@ -59,7 +59,7 @@ export default (async () => {
         input:
             appEnv != 'test'
                 ? ['src/' + appName + '.js', 'src/dbp-template-activity.js']
-                : glob.sync('test/**/*.js'),
+                : globSync('test/**/*.js'),
         output: {
             dir: 'dist',
             entryFileNames: '[name].js',
