56b3ae80bcd1912cc3c469e847c2aa92cf37560a
Patrizio Bekerle <patrizio@bekerle.com>
2024-01-24 14:53:26 +0100

    add: custom element definition for dbp-translated

--- src/dbp-frontend-template-app.js
+++ src/dbp-frontend-template-app.js
@@ -1,4 +1,7 @@
+import '@webcomponents/scoped-custom-element-registry';
 import {AppShell} from '@dbp-toolkit/app-shell';
 import * as commonUtils from '@dbp-toolkit/common/utils';
+import {Translated} from '@dbp-toolkit/common/src/translated';
 
 commonUtils.defineCustomElement('dbp-frontend-template-app', AppShell);
+commonUtils.defineCustomElement('dbp-translated', Translated);
